94f5ecd48b781852b7df8630bef6f597ee424436
Klemen Vodopivec <vodopiveck@ornl.gov>
2014-05-14 13:07:30 -0400

    New FemPlugin
    Added support for FEM10 module. Status readout and configuration support
    exported through PVs.

---
+++ ned/nedApp/src/FemPlugin.h
@@ -0,0 +1,87 @@
+#ifndef FEM_PLUGIN_H
+#define FEM_PLUGIN_H
+
+#include "BaseModulePlugin.h"
+
+/**
+ * Plugin for FEM module.
+ *
+ * General plugin parameters:
+ * asyn param    | asyn param type | init val | mode | Description                   |
+ * ------------- | --------------- | -------- | ---- | ------------------------------
+ * HwDate        | asynOctet       | ""       | RO   | Hardware date as YYYY/MM/DD
+ * HwVer         | asynParamInt32  | 0        | RO   | Hardware version
+ * HwRev         | asynParamInt32  | 0        | RO   | Hardware revision
+ * FwVer         | asynParamInt32  | 0        | RO   | Firmware version
+ * FwRev         | asynParamInt32  | 0        | RO   | Firmware revision
+ */
+class FemPlugin : public BaseModulePlugin {
+    private: // structures and definitions
+        static const unsigned NUM_FEMPLUGIN_STATUSPARAMS;   //!< This is used as a runtime assert check and must match number of status parameters
+
+    private: // variables
+        std::string m_version;              //!< Version string as passed to constructor
+
+    public: // functions
+
+        /**
+         * Constructor for FemPlugin
+         *
+         * Constructor will create and populate PVs with default values.
+         *
+	     * @param[in] portName asyn port name.
+	     * @param[in] dispatcherPortName Name of the dispatcher asyn port to connect to.
+	     * @param[in] hardwareId Hardware ID of the ROC module, can be in IP format (xxx.xxx.xxx.xxx) or
+         *                       in hex number string in big-endian byte order (0x15FACB2D equals to IP 21.250.203.45)
+         * @param[in] version FEM HW&SW version, ie. V10_50
+         * @param[in] blocking Flag whether the processing should be done in the context of caller thread or in background thread.
+         */
+        FemPlugin(const char *portName, const char *dispatcherPortName, const char *hardwareId, const char *version, int blocking=0);
+
+    private: // functions
+        /**
+         * Verify the DISCOVER response is from ROC.
+         *
+         * @param[in] packet with response to DISCOVER
+         * @return true if packet was parsed and type of module is ROC.
+         */
+        bool rspDiscover(const DasPacket *packet);
+
+        /**
+         * Overrided READ_VERSION handler dispatches real work to one of rspReadVersion_*
+         *
+         * @param[in] packet with response to READ_VERSION
+         * @return true if packet was parsed and module version verified.
+         */
+        bool rspReadVersion(const DasPacket *packet);
+
+        /**
+         * Handler for READ_VERSION response from FEM V10
+         *
+         * Populate hardware info parameters, like HwVer, HwRev, FwVer etc.
+         * @relates rspReadVersion
+         */
+        bool rspReadVersion_V10(const DasPacket *packet);
+
+        /**
+         * Create and register all status FEM V10 parameters to be exposed to EPICS.
+         */
+        void createStatusParams_V10();
+
+        /**
+         * Create and register all config FEM V10 parameters to be exposed to EPICS.
+         */
+        void createConfigParams_V10();
+
+    private: // asyn parameters
+        #define FIRST_FEMPLUGIN_PARAM HardwareVer
+        int HardwareVer;    //!< Module hardware version
+        int HardwareRev;    //!< Module hardware revision
+        int HardwareDate;   //!< Module hardware date
+        int FirmwareVer;    //!< Module firmware version
+        int FirmwareRev;    //!< Module firmware revision
+        int FirmwareDate;   //!< Module firmware date
+        #define LAST_FEMPLUGIN_PARAM FirmwareDate
+};
+
+#endif // DSP_PLUGIN_H
