966a2fd6794bd37ca584825c03c0aeee9097831c
Klemen Vodopivec vodopiveck@ornl.gov <vodopiveck@ornl.gov>
2014-06-14 02:28:26 -0400

    Let locally copied buffer not stall but rather delegate that event to OCC API.
    When the local buffer was getting full, the copy thread would quit
    and a stall signal would be sent to consumers. This imposed two possible ways
    to stall, one in the OCC hw and one in software.
    With the patch in place, the copy thread now checks to see if there's place and
    it gives more time to consumers otherwise. It never stalls. Instead it pushes
    stalling back to OCC. If OCC has room all is fine, otherwise OCC stalls which
    is handled already with the status checking.
    The patch is necessary for a piped OCC whichs is feed the data from a file,
    obviously very fast. It also reduces redundant information visible to the user.

--- ned/nedApp/src/DmaCopier.cpp
+++ ned/nedApp/src/DmaCopier.cpp
@@ -1,6 +1,10 @@
 #include "DmaCopier.h"
 
+#include <epicsThread.h>
+#include <errno.h>
 #include <occlib.h>
+
+#define THREAD_INTERVAL           0.1   // Thread resolution time to exit in seconds
 
 DmaCopier::DmaCopier(struct occ_handle *occ, uint32_t bufferSize)
     : CircularBuffer(bufferSize)
@@ -24,12 +28,23 @@ void DmaCopier::run()
         size_t len;
         int status;
 
-        status = occ_data_wait(m_occ, &data, &len, 0);
+        if (full()) {
+            // There's no space in circular buffer, give consumers some extra
+            // time and retry. Hopefully the OCC can host data for a while.
+            // Otherwise a stall condition will occur which is detected in
+            // OccPortDriver.
+            wakeUpConsumer(0);
+            epicsThreadSleep(THREAD_INTERVAL);
+            continue;
+        }
+
+        status = occ_data_wait(m_occ, &data, &len, THREAD_INTERVAL*1000);
+        if (status == -ETIME || len == 0)
+            continue;
         if (status != 0) {
             wakeUpConsumer(status);
             break;
-        } else if (len == 0)
-            continue;
+        }
 
         len = CircularBuffer::push(data, len);
         if (len == 0)
@@ -41,4 +56,5 @@ void DmaCopier::run()
             break;
         }
     }
+    wakeUpConsumer(-ESHUTDOWN);
 }
