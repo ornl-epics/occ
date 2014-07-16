2d69281262a9f6a34e33e49dbfcccad92d9e401d
Klemen Vodopivec <kvx@bl16b-ned.sns.gov>
2014-07-16 15:16:52 -0400

    Data analysis tools
    Two tools to analyze data:
    * occ2csv formats raw OCC into CSV
    * sms2csv formats SMS input stream into CSV

---
+++ DataAnalyze/occ2csv.cpp
@@ -0,0 +1,120 @@
+/**
+ * The occ2csv reads raw OCC stream from either input file or stdin. Each packet
+ * is processed and formated into CSV format. It's finally printed to output file
+ * or stdout.
+ */
+
+#include <fcntl.h>
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+
+#include <string>
+#include <iostream>
+
+#define BUFFER_SIZE     (18000)     // Max payload sent by DSP is 3600
+
+struct occ_header {
+    uint32_t dest_id;
+    uint32_t src_id;
+    uint32_t info;
+    uint32_t length;
+    uint32_t reserved1;
+    uint32_t reserved2;
+};
+
+static void usage(const char *progname) {
+    printf("Usage: %s [OPTION]\n", progname);
+    printf("Convert raw OCC data stream into CSV format.\n");
+    printf("\n");
+    printf("Options:\n");
+    printf("  -i, --input-file FILE   Full path to file to be read or - for stdin\n");
+    printf("  -o, --output-file FILE  Full path fo file to be writen to or - for stdout\n");
+    printf("\n");
+    printf("Example output: %s < occ-dump.raw\n", progname);
+    printf("ID;Destination;Source;Cmdinfo;Length;Subpacket id;Timestamp\n");
+    printf("1;0x000F10CC;0x15FABD04;0x0000DC0C;3600;NEUTRON;220;772645950.988011666\n");
+    printf("2;0x000F10CC;0x15FABD04;0x0000DD0C;3600;NEUTRON;221;772645950.988011666\n");
+    printf("3;0x00000000;0x15FABD04;0x80000085;128;RTDL(cmd);0;772645951.021344666\n");
+    printf("4;0x000F10CC;0x15FABD04;0x200000FF;128;RTDL(data);0;772645951.021344666\n");
+    printf("5;0x000F10CC;0x15FABD04;0x0000DE0C;3600;NEUTRON;222;772645950.988011666\n");
+    printf("6;0x000F10CC;0x15FABD04;0x0000DF0C;3600;NEUTRON;223;772645950.988011666\n");
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+    const char *infile = NULL;
+    const char *outfile = NULL;
+    int ret;
+    struct occ_header header;
+    FILE *infd, *outfd;
+    uint32_t packet_id = 0;
+    uint32_t buf[BUFFER_SIZE];
+
+    for (int i = 1; i < argc; i++) {
+        const char *key = argv[i];
+
+        if (strncmp(key, "-h", 2) == 0 || strncmp(key, "--help", 6) == 0) {
+            usage(argv[0]);
+            return 1;
+        }
+        if (strncmp(key, "-i", 2) == 0 || strncmp(key, "--input-file", 12) == 0) {
+            if ((i + 1) >= argc)
+                break;
+            infile = argv[++i];
+        }
+        if (strncmp(key, "-o", 2) == 0 || strncmp(key, "--output-file", 13) == 0) {
+            if ((i + 1) >= argc)
+                break;
+            outfile = argv[++i];
+        }
+    }
+    if (infile == NULL || std::string(infile) == "-") {
+        infd = stdin;
+    } else {
+        infd = fopen(infile, "r");
+        if (infd == NULL) {
+            std::cerr << "ERROR: cannot open input file" << std::endl;
+            return 3;
+        }
+    }
+
+    if (outfile == NULL || std::string(outfile) == "-") {
+        outfd = stdout;
+    } else {
+        outfd = fopen(outfile, "w+");
+        if (outfd == NULL) {
+            std::cerr << "ERROR: cannot open output file" << std::endl;
+            return 3;
+        }
+    }
+
+    fprintf(outfd, "ID;Destination;Source;Cmdinfo;Length;Subpacket id;Timestamp\n");
+
+    while ((ret = fread(&header, sizeof(header), 1, infd)) > 0) {
+        uint16_t subpacket_id = (header.info >> 8) & 0xFFFF; // Only valid for data packets
+
+        if (header.length & (0x1 << 31)) {
+            // Workaround for the qword FPGA flagging us about dword packet
+            header.length &= ~(0x1 << 31);
+            header.length += 4;
+        }
+
+        ret = fread(buf, 1, header.length, infd);
+        if (ret == -1 && (uint32_t)ret != header.length)
+            break;
+
+        fprintf(outfd, "%u;0x%.08X;0x%.08X;0x%.08X;%u", ++packet_id, header.dest_id, header.src_id, header.info, header.length);
+
+        if ((header.info & 0x80000085) == 0x80000085)       fprintf(outfd, ";RTDL(cmd);0;%u.%09u\n",    buf[0], buf[1]);
+        else if ((header.info & 0x200000FF) == 0x200000FF)  fprintf(outfd, ";RTDL(data);0;%u.%09u\n",   buf[0], buf[1]);
+        else if ((header.info & 0x80000084) == 0x80000084)  fprintf(outfd, ";TSYNC;0;0\n");
+        else if (header.info & 0x80000000)                  fprintf(outfd, ";CMD(0x%.02X);0;%u.%09u\n", header.info & 0xFF, 0, 0);
+        else if ((header.info & 0x4) == 0x4)                fprintf(outfd, ";NEUTRON;%u;%u.%09u\n",     subpacket_id, buf[0], buf[1]);
+        else                                                fprintf(outfd, ";META;%u;%u.%09u\n",        subpacket_id, buf[0], buf[1]);
+    }
+
+    fclose(outfd);
+
+    return 0;
+}
---
+++ DataAnalyze/sms2csv.cpp
@@ -0,0 +1,122 @@
+/**
+ * sms2csv converts PreProcessor ADARA stream into a CSV stream.
+ */
+
+#include <fcntl.h>
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+
+#include <string>
+#include <iostream>
+
+#define BUFFER_SIZE     (10*1024*1024)  // What's the SMS upper limit? Not documented
+
+static void usage(const char *progname) {
+    printf("Usage: %s [OPTION]\n", progname);
+    printf("Convert SMS input stream into a CSV output\n");
+    printf("\n");
+    printf("Options:\n");
+    printf("  -i, --input-file FILE   Full path to file to be read or - for stdin\n");
+    printf("  -o, --output-file FILE  Full path fo file to be writen to or - for stdout\n");
+    printf("\n");
+    printf("Example output: %s < sms_stream.raw\n", progname);
+    printf("ID;Timestamp;Length;Type;Source;Total count;Sub count;EOP\n");
+    printf("1;0.000000000;0;HEARTBEAT\n");
+    printf("2;0.000000000;0;HEARTBEAT\n");
+    printf("3;771516938.022997666;120;RTDL\n");
+    printf("4;771516938.006331666;3600;DATA;4;1634;24;1\n");
+    printf("5;771516938.006331666;3600;DATA;4;97;0;0\n");
+    printf("6;771516938.006331666;3600;DATA;4;99;1;0\n");
+    printf("7;771516938.006331666;3600;DATA;4;101;2;0\n");
+    printf("8;771516937.972998666;24;DATA;4;103;3;0\n");
+    printf("9;771516937.989664666;24;DATA;0;1638;1;1\n");
+    printf("10;771516938.006331666;3600;DATA;0;5;0;0\n");
+    printf("\n");
+}
+
+struct sms_header {
+    uint32_t length;
+    uint32_t type;
+    uint32_t time_sec;
+    uint32_t time_nsec;
+};
+
+int main(int argc, char **argv) {
+    const char *infile = NULL;
+    const char *outfile = NULL;
+    FILE *infd, *outfd;
+    uint32_t packet_id = 0;
+    uint32_t payload[BUFFER_SIZE/4];
+    int ret = 0;
+    struct sms_header header;
+
+    for (int i = 1; i < argc; i++) {
+        const char *key = argv[i];
+
+        if (strncmp(key, "-h", 2) == 0 || strncmp(key, "--help", 6) == 0) {
+            usage(argv[0]);
+            return 1;
+        }
+        if (strncmp(key, "-i", 2) == 0 || strncmp(key, "--input-file", 12) == 0) {
+            if ((i + 1) >= argc)
+                break;
+            infile = argv[++i];
+        }
+        if (strncmp(key, "-o", 2) == 0 || strncmp(key, "--output-file", 13) == 0) {
+            if ((i + 1) >= argc)
+                break;
+            outfile = argv[++i];
+        }
+    }
+    if (infile == NULL || std::string(infile) == "-") {
+        infd = stdin;
+    } else {
+        infd = fopen(infile, "r");
+        if (infd == NULL) {
+            std::cerr << "ERROR: cannot open input file" << std::endl;
+            return 3;
+        }
+    }
+
+    if (outfile == NULL || std::string(outfile) == "-") {
+        outfd = stdout;
+    } else {
+        outfd = fopen(outfile, "w+");
+        if (outfd == NULL) {
+            std::cerr << "ERROR: cannot open output file" << std::endl;
+            return 3;
+        }
+    }
+
+    fprintf(outfd, "ID;Timestamp;Length;Type;Source;Total count;Sub count;EOP\n");
+
+    while ((ret = fread(&header, sizeof(header), 1, infd)) > 0) {
+        // Only valid for DATA type
+        uint32_t source = payload[0];
+        bool eop = (payload[1] >> 31);
+        uint16_t subpacket_cnt = (payload[1] >> 16) & 0x7FF;
+        uint16_t total_cnt = payload[1] & 0xFFFF;
+
+        if (header.length > sizeof(payload)) {
+            fprintf(stderr, "ERROR: packet exceeds internal buffer (%u > %u)\n", header.length, BUFFER_SIZE);
+            ret = 1;
+            break;
+        }
+
+        ret = fread(payload, header.length, 1, infd);
+        if (ret == -1 && (uint32_t)ret != 1)
+            break;
+
+        fprintf(outfd, "%u;%u.%09u;%u;", ++packet_id, header.time_sec, header.time_nsec, header.length);
+        if (header.type == 0x00000000)      fprintf(outfd, "DATA;%u;%u;%u;%u\n", source, total_cnt, subpacket_cnt, eop);
+        else if (header.type == 0x00000100) fprintf(outfd, "RTDL\n");
+        else if (header.type == 0x00000200) fprintf(outfd, "SOURCE LIST\n");
+        else if (header.type == 0x00400900) fprintf(outfd, "HEARTBEAT\n");
+        else                                fprintf(outfd, "UNSUPPORTED(0x%.08X)\n", header.type);
+    }
+
+    fclose(outfd);
+
+    return ret;
+}
