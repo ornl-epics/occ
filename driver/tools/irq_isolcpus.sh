99adbac2a8d1863d3f94c12299601076ae8bd551
Klemen Vodopivec <vodopiveck@ornl.gov>
2014-09-22 11:32:29 -0400

    Handling of hex values for CPU mask

--- driver/tools/irq_isolcpus.sh
+++ driver/tools/irq_isolcpus.sh
@@ -7,23 +7,25 @@
 # September 2014
 # Klemen Vodopivec <klemen.vodopivec@cosylab.com>
 
-DRIVER=dskfj
+DRIVER=sns_ocb # No '-' in driver name, replace '-' with '_'
 
 all_cpus=`lscpu | grep '^CPU(s):' | awk -F':' '{print $2}' | tr -d ' '`
-all_mask=`echo "obase=16; (2 ^ $all_cpus) - 1" | bc | tr A-F a-f`
+all_mask="0x`echo "obase=16; (2 ^ $all_cpus) - 1" | bc | tr A-F a-f`"
 
 # Find mask of available CPUs by looking at init's mask
 init_pid=`pidof init`
-init_mask=`taskset -p $init_pid | awk -F':' '{print $2}' | tr -d ' '`
+init_mask="0x`taskset -p $init_pid | awk -F':' '{print $2}' | tr -d ' '`"
 let "isol_mask = ~($all_mask & $init_mask) & $all_mask"
 if (($isol_mask == 0)); then
 	echo "No isolated CPU found!"
 	echo "Try using isolcpus=<cpu id> kernel parameter"
 	exit 1
 fi
+init_mask=${init_mask:2} # strip away leading 0x
+isol_mask=`echo "obase=16; $isol_mask" | bc` # convert to hex
 
 # Find drivers interrupts, more than 1 possible
-irqs=`grep "$DRIVER" /proc/interrupts | awk -F':' '{print $1}' | tr -d ' '`
+irqs=`grep "$DRIVER" /proc/interrupts | awk -F':' '{print $1}' | tr -d ' ' | tr '\n' ' '`
 if [ "x$irqs" = "x" ]; then
 	echo "Driver \`$DRIVER' not loaded!"
 	exit 1
@@ -31,7 +33,7 @@ fi
 
 # Set existing IRQ handlers to non-isolated mask
 for file in /proc/irq/[0-9]*/smp_affinity; do
-	echo $init_mask > $file 2>/dev/null
+	echo $init_mask > $file
 done
 
 # Also all future loaded drivers and their IRQs
@@ -44,6 +46,7 @@ done
 
 # Print success message
 cpus=""
+isol_mask="0x$isol_mask"
 for ((cpu=0; cpu<$all_cpus; cpu++)); do
 	if ((((1 << $cpu) & $isol_mask) != 0)); then
 		cpus="$cpus$cpu "
