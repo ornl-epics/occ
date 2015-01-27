eb6146a11ad4bfdf176faa8aa8ef093ab42fc880
Klemen Vodopivec <kvx@ornl.gov>
2015-01-27 12:57:25 -0500

    Add option to print RTDL header in data packet, renamed payload switch to events switch.

--- tools/DataAnalyze/occ2csv.cpp
+++ tools/DataAnalyze/occ2csv.cpp
@@ -30,7 +30,8 @@ static void usage(const char *progname) {
     printf("Options:\n");
     printf("  -i, --input-file FILE   Full path to file to be read or - for stdin\n");
     printf("  -o, --output-file FILE  Full path fo file to be writen to or - for stdout\n");
-    printf("  -p, --payload           Print payload as 32 bit HEX numbers\n");
+    printf("  -e, --events            Print events as 32 bit HEX numbers\n");
+    printf("  -r, --rtdl              Print RTDL header as 32 bit HEX when available\n");
     printf("\n");
     printf("Example output: %s < occ-dump.raw\n", progname);
     printf("ID;Destination;Source;Cmdinfo;Length;Subpacket id;Timestamp\n");
@@ -51,7 +52,8 @@ int main(int argc, char **argv) {
     FILE *infd, *outfd;
     uint32_t packet_id = 0;
     uint32_t buf[BUFFER_SIZE];
-    int payload = 0;
+    int events = 0;
+    int rtdl = 0;
 
     for (int i = 1; i < argc; i++) {
         const char *key = argv[i];
@@ -60,8 +62,11 @@ int main(int argc, char **argv) {
             usage(argv[0]);
             return 1;
         }
-        if (strncmp(key, "-p", 2) == 0 || strncmp(key, "--payload", 9) == 0) {
-            payload = 1;
+        if (strncmp(key, "-e", 2) == 0 || strncmp(key, "--events", 8) == 0) {
+            events = 1;
+        }
+        if (strncmp(key, "-r", 2) == 0 || strncmp(key, "--rtdl", 6) == 0) {
+            rtdl = 1;
         }
         if (strncmp(key, "-i", 2) == 0 || strncmp(key, "--input-file", 12) == 0) {
             if ((i + 1) >= argc)
@@ -109,7 +114,10 @@ int main(int argc, char **argv) {
         if (ret == -1 && (uint32_t)ret != header.length)
             break;
 
-        fprintf(outfd, "%u;0x%.08X;0x%.08X;0x%.08X;%u", ++packet_id, header.dest_id, header.src_id, header.info, header.length);
+        uint32_t events_len = header.length;
+        if (header.info & 0x8)
+            events_len -= 6*sizeof(uint32_t); // skip RTDL header
+        fprintf(outfd, "%u;0x%.08X;0x%.08X;0x%.08X;%u", ++packet_id, header.dest_id, header.src_id, header.info, events_len);
 
         if ((header.info & 0x80000085) == 0x80000085)       fprintf(outfd, ";RTDL(cmd);0;%u.%09u\n",    buf[0], buf[1]);
         else if ((header.info & 0x200000FF) == 0x200000FF)  fprintf(outfd, ";RTDL(data);0;%u.%09u\n",   buf[0], buf[1]);
@@ -120,7 +128,17 @@ int main(int argc, char **argv) {
         else if ((header.info & 0x8) == 0x8)                fprintf(outfd, ";META;%u;%u.%09u\n",        subpacket_id, buf[0], buf[1]);
         else                                                fprintf(outfd, ";META;%u;missing RTDL\n",   subpacket_id);
 
-        if (payload && (header.info & 0x800000F0) == 0) {
+        if (rtdl && (header.info & 0x80000008) == 0x8) {
+            uint32_t i;
+            for (i = 0; i < 6; i++) {
+                if (i == 0)          fprintf(outfd,   "  ");
+                else if (i % 4 == 0) fprintf(outfd, "\n  ");
+                fprintf(outfd, "%08X ", buf[i]);
+            }
+            fprintf(outfd, "\n");
+        }
+
+        if (events && (header.info & 0x800000F0) == 0) {
             uint32_t skip = (header.info & 0x8) ? 6 : 0;
             uint32_t i = skip;
             for (; i < header.length/4; i++) {
