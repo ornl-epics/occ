60872389e27a59173ccf12ef0c5487f1d7d7adbb
Vodopivec K <kvx@bl11a-daq1.sns.gov>
2016-05-13 13:05:59 -0400

    Revive support for legacy PCI-X OCC
    Legacy OCC doesn't like late RX enable. Add a meta flag to the device
    description and some code to not support late RX enable for legacy stuff.

--- driver/sns-ocb.c
+++ driver/sns-ocb.c
@@ -203,6 +203,7 @@ struct ocb_board_desc {
 	u32 unified_que;
 	u32 bars[3];
 	u32 reset_errcnt;	// Does board have support for resetting error counters?
+	u32 late_rx_enable;	// Does board support late RX enable?
 	struct attribute_group sysfs;
 };
 
@@ -302,6 +303,7 @@ static struct ocb_board_desc boards[] = {
 		.unified_que = 1,
 		.bars = { 1048576, 1048576 },
 		.reset_errcnt = 0,
+		.late_rx_enable = 0,
 	},
 	{
 		.type = BOARD_SNS_PCIX,
@@ -310,6 +312,7 @@ static struct ocb_board_desc boards[] = {
 		.unified_que = 0,
 		.bars = { 1048576, 1048576 },
 		.reset_errcnt = 0,
+		.late_rx_enable = 0,
 	},
 	{
 		.type = BOARD_SNS_PCIE,
@@ -318,6 +321,7 @@ static struct ocb_board_desc boards[] = {
 		.unified_que = 1,
 		.bars = { 4096, 32768, 16777216 },
 		.reset_errcnt = 1,
+		.late_rx_enable = 1,
 	},
 	{
 		.type = BOARD_SNS_PCIE,
@@ -326,6 +330,7 @@ static struct ocb_board_desc boards[] = {
 		.unified_que = 1,
 		.bars = { 4096, 32768, 16777216 },
 		.reset_errcnt = 1,
+		.late_rx_enable = 1,
 		.sysfs.attrs = (struct attribute **) (struct device_attribute *[]){
 			SNSOCB_DEVICE_ATTR("irq_coalescing", 0644, snsocb_sysfs_show_irq_coallesce, snsocb_sysfs_store_irq_coallesce),
 			SNSOCB_DEVICE_ATTR("dma_big_mem", 0644, snsocb_sysfs_show_dma_big_mem, snsocb_sysfs_store_dma_big_mem),
@@ -913,7 +918,10 @@ static void snsocb_reset(struct ocb *ocb)
 	else
 		iowrite32(0x0, ioaddr + REG_DQ_CONS_INDEX);
 
-	ocb->conf &= ~OCB_CONF_RX_ENABLE;
+	/* Some boards like legacy PCI-X do not like to get spontaneously enabled */
+	if (ocb->board->late_rx_enable)
+		ocb->conf &= ~OCB_CONF_RX_ENABLE;
+
 	iowrite32(ocb->conf, ocb->ioaddr + REG_CONFIG);
 	if (ocb->emulate_dq) {
 		ocb->dq_prod = ocb->dq_cons = 0;
@@ -1251,6 +1259,15 @@ static ssize_t snsocb_write(struct file *file, const char __user *buf,
 
 		if (copy_from_user(&val, buf, sizeof(u32)))
 			return -EFAULT;
+
+		if (!ocb->board->late_rx_enable) {
+			/* When board does not support enabling/disabling RX,
+			   it always enabled. Allow enabling but scream if
+			   tried to disable. */
+			if (!val)
+				return -EINVAL;
+			break;
+		}
 
 		spin_lock_irq(&ocb->lock);
 		if (ocb->reset_in_progress) {
@@ -1633,8 +1650,7 @@ static int __devexit snsocb_probe(struct pci_dev *pdev,
 		}
         }
 
-	err = devm_request_irq(dev, pdev->irq, snsocb_interrupt, IRQF_SHARED,
-			       KBUILD_MODNAME, ocb);
+	err = request_irq(pdev->irq, snsocb_interrupt, IRQF_SHARED, KBUILD_MODNAME, ocb);
 	if (err) {
 		dev_err(dev, "unable to request interrupt, aborting");
 		goto error_dev;
@@ -1744,11 +1760,13 @@ static void __devexit snsocb_remove(struct pci_dev *pdev)
 	if (ocb && ocb->board && ocb->board->sysfs.attrs)
 		sysfs_remove_group(&dev->kobj, &ocb->board->sysfs);
 
-	device_del(&ocb->dev);
-	cdev_del(&ocb->cdev);
-
 	iowrite32(0, ocb->ioaddr + REG_IRQ_ENABLE);
 	iowrite32(OCB_CONF_RESET, ocb->ioaddr + REG_CONFIG);
+	ioread32(ocb->ioaddr + REG_IRQ_ENABLE); // Make sure device got the iowrite32()
+
+	free_irq(pdev->irq, ocb);
+	device_del(&ocb->dev);
+	cdev_del(&ocb->cdev);
 
 	snsocb_free_big_queue(ocb);
 	snsocb_free_queue(dev, ocb->dq_page, ocb->dq_dma, OCB_DQ_SIZE);
