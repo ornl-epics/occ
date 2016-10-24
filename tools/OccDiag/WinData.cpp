f1f474b475b8165f6575a8d4af0d2197ee1e1dfe
Klemen Vodopivec <kvx@ornl.gov>
2016-10-24 12:44:02 -0400

    bugfix: Don't display data if no address has been set

--- tools/OccDiag/WinData.cpp
+++ tools/OccDiag/WinData.cpp
@@ -15,42 +15,44 @@ void WinData::redraw(bool frame)
     if (!m_window)
         return;
 
-    int width, height;
-    getSize(width, height);
+    if (m_addrPacket) {
+        int width, height;
+        getSize(width, height);
 
-    // Calculate starting address - display 3 lines of previous packet unless
-    // error address can't fit on the same screen
-    const void *addr = (const char *)m_addrPacket - 3*dwordsPerLine*sizeof(uint32_t);
-    if (m_addrError) {
-        int line = 3 + (uint32_t)((const char *)m_addrError - (const char *)m_addrPacket) / (dwordsPerLine*sizeof(uint32_t));
-        if (line > height) {
-            addr = (const char *)m_addrError - (height-2)*dwordsPerLine*sizeof(uint32_t);
-        }
-    }
-    addr = (const char *)addr + m_lineOffset*4*sizeof(uint32_t);
-
-    const DasPacket *packet = (const DasPacket *)m_addrPacket;
-    size_t packetLen = packet->length();
-    if (packetLen == 0)
-        packetLen = m_size;
-
-    // Print memory address followed by 4 dwords in hex
-    for (int i=0; i<height; i++) {
-        uint8_t pos = 19;
-        mvwprintw(m_window, i+1, 1, "%016p: ", addr);
-
-        for (size_t j=0; j<dwordsPerLine; j++) {
-            if (addr >= m_addrBase && addr < ((const char *)m_addrBase + m_size)) {
-                if (addr == m_addrError) {
-                    mvwprintw_c(m_window, TEXT_COLOR_RED, i+1, pos, "0x%08X ", *(const uint32_t *)addr);
-                } else if (addr >= m_addrPacket && addr < ((const char *)m_addrPacket + packetLen)) {
-                    mvwprintw_c(m_window, TEXT_COLOR_YELLOW, i+1, pos, "0x%08X ", *(const uint32_t *)addr);
-                } else {
-                    mvwprintw(m_window, i+1, pos, "0x%08X ", *(const uint32_t *)addr);
-                }
+        // Calculate starting address - display 3 lines of previous packet unless
+        // error address can't fit on the same screen
+        const void *addr = (const char *)m_addrPacket - 3*dwordsPerLine*sizeof(uint32_t);
+        if (m_addrError) {
+            int line = 3 + (uint32_t)((const char *)m_addrError - (const char *)m_addrPacket) / (dwordsPerLine*sizeof(uint32_t));
+            if (line > height) {
+                addr = (const char *)m_addrError - (height-2)*dwordsPerLine*sizeof(uint32_t);
             }
-            pos += 11;
-            addr = (const char *)addr + 4;
+        }
+        addr = (const char *)addr + m_lineOffset*4*sizeof(uint32_t);
+
+        const DasPacket *packet = (const DasPacket *)m_addrPacket;
+        size_t packetLen = packet->length();
+        if (packetLen == 0)
+            packetLen = m_size;
+
+        // Print memory address followed by 4 dwords in hex
+        for (int i=0; i<height; i++) {
+            uint8_t pos = 19;
+            mvwprintw(m_window, i+1, 1, "%016p: ", addr);
+
+            for (size_t j=0; j<dwordsPerLine; j++) {
+                if (addr >= m_addrBase && addr < ((const char *)m_addrBase + m_size)) {
+                    if (addr == m_addrError) {
+                        mvwprintw_c(m_window, TEXT_COLOR_RED, i+1, pos, "0x%08X ", *(const uint32_t *)addr);
+                    } else if (addr >= m_addrPacket && addr < ((const char *)m_addrPacket + packetLen)) {
+                        mvwprintw_c(m_window, TEXT_COLOR_YELLOW, i+1, pos, "0x%08X ", *(const uint32_t *)addr);
+                    } else {
+                        mvwprintw(m_window, i+1, pos, "0x%08X ", *(const uint32_t *)addr);
+                    }
+                }
+                pos += 11;
+                addr = (const char *)addr + 4;
+            }
         }
     }
 
